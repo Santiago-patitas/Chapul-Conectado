e75e2308c38637e8e1fbd5eea205dcfed80ec97e
Santiago-patitas <24301699@uttt.edu.mx>
2025-03-28 08:25:44 -0600

    Add files via upload
    ice todo

---
+++ calculos.js
@@ -0,0 +1,333 @@
+function Sueldo1()
+{
+    var sxd=0;
+    var dt=0;
+    var b1=0;
+    var b2=0;
+    var b3=0;
+    var d1=0;
+    var d2=0;
+    var dto=0;
+    var sl=0;
+    var sd=0;
+    var sn=0;
+
+    sxd=parseInt(document.getElementById("sueldod1").value);
+    dt=parseInt(document.getElementById("dias1").value);
+    b1=parseInt(document.getElementById("bono1").value);
+    b2=parseInt(document.getElementById("bono12").value);
+    b3=parseInt(document.getElementById("bono13").value);
+    d1=parseInt(document.getElementById("descuento1").value);
+    d2=parseInt(document.getElementById("descuento12").value);
+
+    
+    sl=(sxd*dt)+b1+b2+b3;
+    dto=d1+d2;
+    sd=(sl*dto)/100;
+    sn=sl-sd;
+    
+    document.getElementById("sueldon1").value=sn;
+
+}
+function Sueldo2()
+{
+    var sxd=0;
+    var dt=0;
+    var b1=0;
+    var b2=0;
+    var b3=0;
+    var d1=0;
+    var d2=0;
+    var dto=0;
+    var sl=0;
+    var sd=0;
+    var sn=0;
+
+    sxd=parseInt(document.getElementById("sueldod2").value);
+    dt=parseInt(document.getElementById("dias1").value);
+    b1=parseInt(document.getElementById("bono2").value);
+    b2=parseInt(document.getElementById("bono22").value);
+    b3=parseInt(document.getElementById("bono23").value);
+    d1=parseInt(document.getElementById("descuento2").value);
+    d2=parseInt(document.getElementById("descuento22").value);
+
+    
+    sl=(sxd*dt)+b1+b2+b3;
+    dto=d1+d2;
+    sd=(sl*dto)/100;
+    sn=sl-sd;
+    
+    document.getElementById("sueldon2").value=sn;
+}
+function Sueldo3()
+{
+    var sxd=0;
+    var dt=0;
+    var b1=0;
+    var b2=0;
+    var b3=0;
+    var d1=0;
+    var d2=0;
+    var dto=0;
+    var sl=0;
+    var sd=0;
+    var sn=0;
+
+    sxd=parseInt(document.getElementById("sueldod3").value);
+    dt=parseInt(document.getElementById("dias1").value);
+    b1=parseInt(document.getElementById("bono3").value);
+    b2=parseInt(document.getElementById("bono32").value);
+    b3=parseInt(document.getElementById("bono33").value);
+    d1=parseInt(document.getElementById("descuento3").value);
+    d2=parseInt(document.getElementById("descuento32").value);
+
+    
+    sl=(sxd*dt)+b1+b2+b3;
+    dto=d1+d2;
+    sd=(sl*dto)/100;
+    sn=sl-sd;
+    
+    document.getElementById("sueldon3").value=sn;
+}
+function Sueldo4()
+{
+    var sxd=0;
+    var dt=0;
+    var b1=0;
+    var b2=0;
+    var b3=0;
+    var d1=0;
+    var d2=0;
+    var dto=0;
+    var sl=0;
+    var sd=0;
+    var sn=0;
+
+    sxd=parseInt(document.getElementById("sueldod4").value);
+    dt=parseInt(document.getElementById("dias1").value);
+    b1=parseInt(document.getElementById("bono4").value);
+    b2=parseInt(document.getElementById("bono42").value);
+    b3=parseInt(document.getElementById("bono43").value);
+    d1=parseInt(document.getElementById("descuento4").value);
+    d2=parseInt(document.getElementById("descuento42").value);
+
+    
+    sl=(sxd*dt)+b1+b2+b3;
+    dto=d1+d2;
+    sd=(sl*dto)/100;
+    sn=sl-sd;
+    
+    document.getElementById("sueldon4").value=sn;
+}
+function Sueldo5()
+{
+    var sxd=0;
+    var dt=0;
+    var b1=0;
+    var b2=0;
+    var b3=0;
+    var d1=0;
+    var d2=0;
+    var dto=0;
+    var sl=0;
+    var sd=0;
+    var sn=0;
+
+    sxd=parseInt(document.getElementById("sueldod5").value);
+    dt=parseInt(document.getElementById("dias1").value);
+    b1=parseInt(document.getElementById("bono5").value);
+    b2=parseInt(document.getElementById("bono52").value);
+    b3=parseInt(document.getElementById("bono53").value);
+    d1=parseInt(document.getElementById("descuento5").value);
+    d2=parseInt(document.getElementById("descuento52").value);
+
+    
+    sl=(sxd*dt)+b1+b2+b3;
+    dto=d1+d2;
+    sd=(sl*dto)/100;
+    sn=sl-sd;
+    
+    document.getElementById("sueldon5").value=sn;
+}
+function Sueldo6()
+{
+    var sxd=0;
+    var dt=0;
+    var b1=0;
+    var b2=0;
+    var b3=0;
+    var d1=0;
+    var d2=0;
+    var dto=0;
+    var sl=0;
+    var sd=0;
+    var sn=0;
+
+    sxd=parseInt(document.getElementById("sueldod6").value);
+    dt=parseInt(document.getElementById("dias1").value);
+    b1=parseInt(document.getElementById("bono6").value);
+    b2=parseInt(document.getElementById("bono62").value);
+    b3=parseInt(document.getElementById("bono63").value);
+    d1=parseInt(document.getElementById("descuento6").value);
+    d2=parseInt(document.getElementById("descuento62").value);
+
+    
+    sl=(sxd*dt)+b1+b2+b3;
+    dto=d1+d2;
+    sd=(sl*dto)/100;
+    sn=sl-sd;
+    
+    document.getElementById("sueldon6").value=sn;
+}
+function Sueldo7()
+{
+    var sxd=0;
+    var dt=0;
+    var b1=0;
+    var b2=0;
+    var b3=0;
+    var d1=0;
+    var d2=0;
+    var dto=0;
+    var sl=0;
+    var sd=0;
+    var sn=0;
+
+    sxd=parseInt(document.getElementById("sueldod7").value);
+    dt=parseInt(document.getElementById("dias1").value);
+    b1=parseInt(document.getElementById("bono7").value);
+    b2=parseInt(document.getElementById("bono72").value);
+    b3=parseInt(document.getElementById("bono73").value);
+    d1=parseInt(document.getElementById("descuento7").value);
+    d2=parseInt(document.getElementById("descuento72").value);
+
+    
+    sl=(sxd*dt)+b1+b2+b3;
+    dto=d1+d2;
+    sd=(sl*dto)/100;
+    sn=sl-sd;
+    
+    document.getElementById("sueldon7").value=sn;
+
+}
+function Sueldo8()
+{
+    var sxd=0;
+    var dt=0;
+    var b1=0;
+    var b2=0;
+    var b3=0;
+    var d1=0;
+    var d2=0;
+    var dto=0;
+    var sl=0;
+    var sd=0;
+    var sn=0;
+
+    sxd=parseInt(document.getElementById("sueldod8").value);
+    dt=parseInt(document.getElementById("dias1").value);
+    b1=parseInt(document.getElementById("bono8").value);
+    b2=parseInt(document.getElementById("bono82").value);
+    b3=parseInt(document.getElementById("bono83").value);
+    d1=parseInt(document.getElementById("descuento8").value);
+    d2=parseInt(document.getElementById("descuento82").value);
+
+    
+    sl=(sxd*dt)+b1+b2+b3;
+    dto=d1+d2;
+    sd=(sl*dto)/100;
+    sn=sl-sd;
+    
+    document.getElementById("sueldon8").value=sn;
+
+}
+function Sueldo9()
+{
+    var sxd=0;
+    var dt=0;
+    var b1=0;
+    var b2=0;
+    var b3=0;
+    var d1=0;
+    var d2=0;
+    var dto=0;
+    var sl=0;
+    var sd=0;
+    var sn=0;
+
+    sxd=parseInt(document.getElementById("sueldod9").value);
+    dt=parseInt(document.getElementById("dias1").value);
+    b1=parseInt(document.getElementById("bono9").value);
+    b2=parseInt(document.getElementById("bono92").value);
+    b3=parseInt(document.getElementById("bono93").value);
+    d1=parseInt(document.getElementById("descuento9").value);
+    d2=parseInt(document.getElementById("descuento92").value);
+
+    
+    sl=(sxd*dt)+b1+b2+b3;
+    dto=d1+d2;
+    sd=(sl*dto)/100;
+    sn=sl-sd;
+    
+    document.getElementById("sueldon9").value=sn;
+}
+function Sueldo10()
+{
+    var sxd=0;
+    var dt=0;
+    var b1=0;
+    var b2=0;
+    var b3=0;
+    var d1=0;
+    var d2=0;
+    var dto=0;
+    var sl=0;
+    var sd=0;
+    var sn=0;
+
+    sxd=parseInt(document.getElementById("sueldod10").value);
+    dt=parseInt(document.getElementById("dias1").value);
+    b1=parseInt(document.getElementById("bono10").value);
+    b2=parseInt(document.getElementById("bono102").value);
+    b3=parseInt(document.getElementById("bono103").value);
+    d1=parseInt(document.getElementById("descuento10").value);
+    d2=parseInt(document.getElementById("descuento102").value);
+
+    
+    sl=(sxd*dt)+b1+b2+b3;
+    dto=d1+d2;
+    sd=(sl*dto)/100;
+    sn=sl-sd;
+    
+    document.getElementById("sueldon10").value=sn;
+}
+function Total()
+{
+    var sn1=0;
+    var sn2=0;
+    var sn3=0;
+    var sn4=0;
+    var sn5=0;
+    var sn6=0;
+    var sn7=0;
+    var sn8=0;
+    var sn9=0;
+    var sn10=0;
+    var total=0;
+
+    sn1=parseInt(document.getElementById("sueldon1").value);
+    sn2=parseInt(document.getElementById("sueldon2").value);
+    sn3=parseInt(document.getElementById("sueldon3").value);
+    sn4=parseInt(document.getElementById("sueldon4").value);
+    sn5=parseInt(document.getElementById("sueldon5").value);
+    sn6=parseInt(document.getElementById("sueldon6").value);
+    sn7=parseInt(document.getElementById("sueldon7").value);
+    sn8=parseInt(document.getElementById("sueldon8").value);
+    sn9=parseInt(document.getElementById("sueldon9").value);
+    sn10=parseInt(document.getElementById("sueldon10").value);
+
+    var total=sn1+sn2+sn3+sn4+sn5+sn6+sn7+sn8+sn9+sn10;
+
+    document.getElementById("sueldot").value=total;
+
+}
